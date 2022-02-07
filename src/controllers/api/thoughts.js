b6c3a36e483d534031a3971db3194143e3c463ce
Conor Kelly <conorjkelly96@gmail.com>
2022-02-07 21:40:41 +0000

    Syntax Refactoring

--- src/controllers/api/thoughts.js
+++ src/controllers/api/thoughts.js
@@ -2,9 +2,8 @@ const { Thought } = require("../../models");
 
 const getThoughts = async (req, res) => {
   try {
-    res.send("getThought");
-    // const thoughts = await User.find({});
-    // return res.json({ success: true, data: students });
+    const thoughts = await Thought.find({});
+    return res.json({ success: true, data: thoughts });
   } catch (error) {
     console.log(`[ERROR]: Failed to get Thoughts | ${error.message}`);
     return res
@@ -15,10 +14,9 @@ const getThoughts = async (req, res) => {
 
 const getThoughtById = async (req, res) => {
   try {
-    res.send("getThoughtById");
-    // const { userId } = req.params;
-    // const User = await User.findById(studentId);
-    // return res.json({ success: true, data: student });
+    const { thoughtId } = req.params;
+    const thought = await Thought.findById(thoughtId);
+    return res.json({ success: true, data: thought });
   } catch (error) {
     console.log(`[ERROR]: Failed to get Thought | ${error.message}`);
     return res
@@ -27,20 +25,56 @@ const getThoughtById = async (req, res) => {
   }
 };
 
-const createThought = (req, res) => {
-  res.send("createThought");
+const createThought = async (req, res) => {
+  try {
+    const { thoughtText, username } = req.body;
+    const newThought = await Thought.create({ thoughtText, username });
+    return res.json({ success: true, data: newThought });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to create Thought | ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to create Thought" });
+  }
 };
 
-const updateThoughtById = (req, res) => {
-  res.send("updateThoughtById");
+const updateThoughtById = async (req, res) => {
+  try {
+    const { thoughtId } = req.params;
+    const data = req.body;
+
+    const updateThought = await Thought.findByIdAndUpdate(thoughtId, data);
+
+    if (!updateThought) {
+      return res.json({ success: false, message: "Thought does not exist" });
+    }
+
+    return res.json({ success: true, data: updateThought });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to create Thought | ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to create Thought" });
+  }
 };
 
-const deleteThoughtById = (req, res) => {
-  // const { UserId } = req.params;
+const deleteThoughtById = async (req, res) => {
+  try {
+    const { thoughtId } = req.params;
 
-  // console.log("UserId", UserId);
+    const deleteThought = await Thought.findByIdAndDelete(thoughtId);
 
-  res.send("deleteThoughtById");
+    if (!deleteThought) {
+      return res.json({ success: false, message: "Thought does not exist" });
+    }
+
+    return res.json({ success: true, data: deleteThought });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to create Thought | ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to create Thought" });
+  }
 };
 
 module.exports = {
