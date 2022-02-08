7130266b25a2c03221cee3cb12b2b89738abdc61
Conor Kelly <conorjkelly96@gmail.com>
2022-02-08 18:29:06 +0000

    updating user seeds

--- src/schema/data/users.js
+++ src/schema/data/users.js
@@ -2,26 +2,18 @@ const users = [
   {
     username: "partiboi",
     email: "partiboi@gmail.com",
-    thoughts: [],
-    friends: [],
   },
   {
     username: "knoxville33",
     email: "knoxville33@gmail.com",
-    thoughts: [],
-    friends: [],
   },
   {
     username: "frodobaggins",
     email: "frodobaggins@gmail.com",
-    thoughts: [],
-    friends: [],
   },
   {
     username: "dumbledore1757",
     email: "dumbledore1757@gmail.com",
-    thoughts: [],
-    friends: [],
   },
 ];
 
