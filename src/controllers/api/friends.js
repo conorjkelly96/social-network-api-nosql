75af5ac2a396732fc3c1e5b1bc881d976e39fa1b
Conor Kelly <conorjkelly96@gmail.com>
2022-02-10 22:15:41 +0000

    added friends to array

--- src/controllers/api/friends.js
+++ src/controllers/api/friends.js
@@ -1,8 +1,27 @@
-const addNewFriendToUser = (req, res) => {
-  res.send("addNewFriendToUser");
+const { User } = require("../../models");
+
+const addNewFriendToUser = async (req, res) => {
+  try {
+    const { id } = req.body;
+
+    const { userId } = req.params;
+
+    console.log(id, userId);
+
+    const newFriend = await User.findByIdAndUpdate(userId, {
+      $push: { friends: { _id: id } },
+    });
+
+    return res.json({ success: true, data: newFriend });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to create reaction | ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to create reaction" });
+  }
 };
 
-const removeFriendFromUser = (req, res) => {
+const removeFriendFromUser = async (req, res) => {
   res.send("removeFriendFromUser");
 };
 
