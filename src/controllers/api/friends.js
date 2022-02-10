6b2bdfdbe32d331bb62a336a05a8ad1dd0e3655e
Conor Kelly <conorjkelly96@gmail.com>
2022-02-10 22:16:39 +0000

    addNewFriendToUser

--- src/controllers/api/friends.js
+++ src/controllers/api/friends.js
@@ -14,15 +14,30 @@ const addNewFriendToUser = async (req, res) => {
 
     return res.json({ success: true, data: newFriend });
   } catch (error) {
-    console.log(`[ERROR]: Failed to create reaction | ${error.message}`);
+    console.log(`[ERROR]: Failed to add friend | ${error.message}`);
     return res
       .status(500)
-      .json({ success: false, error: "Failed to create reaction" });
+      .json({ success: false, error: "Failed to add friend" });
   }
 };
 
 const removeFriendFromUser = async (req, res) => {
-  res.send("removeFriendFromUser");
+  try {
+    const { thoughtId, reactionId } = req.params;
+
+    console.log(thoughtId, reactionId);
+
+    const reaction = await Thought.findByIdAndUpdate(thoughtId, {
+      $pull: { reactions: { _id: reactionId } },
+    });
+
+    return res.json({ success: true, data: reaction });
+  } catch (error) {
+    console.log(`[ERROR]: Failed to delete reaction | ${error.message}`);
+    return res
+      .status(500)
+      .json({ success: false, error: "Failed to delete reaction" });
+  }
 };
 
 module.exports = {
